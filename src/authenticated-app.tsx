d09d8d627a603b23c3a7d805fafb3dfd1a52e29a
morn <morndemo@163.com>
2022-10-18 11:52:28 +0800

    feat: 用emotion styled 自定义组件样式Row

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,26 +2,24 @@ import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      {/* <Nav>nav</Nav> */}
       <Main>
         <ProjectListScreen />
       </Main>
-      {/* <Aside>aside</Aside>
-      <Footer>footer</Footer> */}
     </Container>
   );
 };
@@ -29,31 +27,9 @@ export const AuthenticatedApp = () => {
 const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem 1fr;
-  /* grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas: 
-  "header header header" "nav main aside" "footer footer footer"; */
   height: 100vh;
 `;
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-  grid-area: main;
-`;
-// const Nav = styled.nav`
-// grid-area: nav;
-// `
-// const Aside = styled.aside`
-// grid-area: aside;
-// `
-// const Footer = styled.footer`
-// grid-area: footer;
-// `
+const Main = styled.main``;
